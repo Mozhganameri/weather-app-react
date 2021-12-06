6925ace710f7d2c0750ea3074250f50eed44a63e
Mozhganameri <86070437+Mozhganameri@users.noreply.github.com>
2021-12-07 02:28:26 +0330

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from "react"
-import './App.css';
+import React from "react";
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      Hi
-      </header>
+      <header className="App-header">Hi</header>
+      <footer>
+        This website was coded by Mozhgan Ameri and is{" "}
+        <a
+          href="https://github.com/Mozhganameri/weather-app-react"
+          target="_blank"
+        >
+          open-sourced
+        </a>{" "}
+        on GitHub.
+      </footer>
     </div>
   );
 }
-
